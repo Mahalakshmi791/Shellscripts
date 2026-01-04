469702d231d3ffa514931421399903ae922167bc
maha <maridayya@gmail.com>
2026-01-05 05:13:08 +0530

    set -e usecase

--- set_function.sh
+++ set_function.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
-trap 'failure  "${LINENO}" "${BASH_COMMAND}"' ERR
-failure(){
+# set -e
+# trap 'failure  "${LINENO}" "${BASH_COMMAND}"' ERR
+# failure(){
 
-echo "script failed at $1-$2"
+# echo "script failed at $1-$2"
 
-}
+# }
 
 echo "Heloo"
 echoooo "hi"
