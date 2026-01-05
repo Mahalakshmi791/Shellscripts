fb5640ebde4b2cd168b851e1485795be09a7f031
maha <maridayya@gmail.com>
2026-01-05 06:18:16 +0530

    delete old logs

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -9,17 +9,32 @@ mkdir -p "$logs_dir"
 
 touch "$logs_dir"/logs
 
-if [! -d "$Source_dir"]
+if [ ! -d "$Source_dir" ]
 then
 echo "$Source_dir not existing"
 exit 1
 else
 
-find "$Source_dir" -name *.log -mtime +30 -delete
+find "$Source_dir" -name "*.log" -mtime +30 -delete
 
-find "$Source_dir" -name *.log -mtime +7 > "$logs_dir"/logs
+find "$Source_dir" -name "*.log" -mtime +7 > "$logs_dir"/logs
+
+if [ -s "$logs_dir"/logs ]
+then
+tar -cvzf "$logs_dir/logs_$(date + %F).tar.gz" "$logs_dir"/logs
+
+while IFS=read -r file
+do
+
+rm -rf "$file"
+
+done < "$logs_dir"/logs
+
+else
+echo "there are no files less than 7 days"
+fi
 
 
-tar -cvzf "$logs_dir"/logs
+rm -rf ""$logs_dir"/logs"
 
 fi
