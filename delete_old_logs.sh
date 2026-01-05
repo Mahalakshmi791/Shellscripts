25bd1203d9c00b4494e4dfa6f8b171766358566c
maha <maridayya@gmail.com>
2026-01-05 06:33:51 +0530

    delete old logs

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -14,26 +14,29 @@ echo "$Source_dir not existing"
 exit 1
 else
 
+# 1. Delete raw log files older than 30 days
 find "$Source_dir" -name "*.log" -mtime +30 -delete
 
+# 2. Collect logs older than 7 days into a list
 find "$Source_dir" -name "*.log" -mtime +7 > "$logs_dir"/logs
 
+#Archive them
 if [ -s "$logs_dir"/logs ]
 then
 tar -cvzf "$logs_dir/logs_$(date +%F_%H-%M-%S).tar.gz" -T "$logs_dir"/logs
 
+#delete originals after archiving
+
 while read -r file
 do
-
 rm -rf "$file"
-
 done < "$logs_dir"/logs
 
 else
 echo "there are no files less than 7 days"
 fi
 
-
+#clean up
 rm -rf ""$logs_dir"/logs"
 
 fi
