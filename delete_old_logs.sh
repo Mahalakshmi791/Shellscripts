d06a6fa2362e5fdf423ead237ea1941d9734d404
maha <maridayya@gmail.com>
2026-01-05 05:33:52 +0530

    delete old logs

---
+++ delete_old_logs.sh
@@ -0,0 +1,23 @@
+#archive logs older than 7 days and delete logs older than 30 days
+
+#!/bin/bash
+
+Source_dir=$1
+logs_dir=$2
+
+mkdir -p logs
+
+if [! -d $Source_dir]
+then
+echo "$Source_dir not existing"
+exit 1
+else
+
+find $Source_dir -name *.log -mtime +30 -delete
+
+find $Source_dir -name *.log -mtime +7 > $logs_dir/logs/
+
+
+tar -cvzf $logs_dir/logs_$(date).gz $logs_dir/logs/
+
+fi
